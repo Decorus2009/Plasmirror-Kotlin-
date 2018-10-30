cda4ac6dce09b8af468dcc5014bfe27e0ae8ac45
Decorus2009 <Decorus2009@mail.ru>
2018-10-31 02:45:42 +0300

    implementing full Mie scattering approach

--- Plasmirror_Kotlin/src/main/kotlin/core/optics/MetallicClusters.kt
+++ Plasmirror_Kotlin/src/main/kotlin/core/optics/MetallicClusters.kt
@@ -3,6 +3,7 @@ package core.optics
 import core.Complex_
 import core.Complex_.Companion.I
 import core.Complex_.Companion.ONE
+import core.Complex_.Companion.ZERO
 import core.Interpolator
 import core.State
 import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction
@@ -77,6 +78,19 @@ object MetallicClusters {
 
     object MieTheory {
 
+        private lateinit var D: MutableList<Complex_>
+        private lateinit var psi: MutableList<Double>
+        private lateinit var xi: List<Complex_>
+        private lateinit var a: MutableList<Complex_>
+        private lateinit var b: MutableList<Complex_>
+
+        private var x = 0.0
+        private var wavevector = 0.0
+        private var radius = 0.0
+        private var nStop = 0
+        private lateinit var m: Complex_
+        private lateinit var mx: Complex_
+
         fun extinctionCoefficient(wavelength: Double, epsMatrix: Complex_, epsMetal: Complex_, f: Double, r: Double) =
                 alphaExtAlphaSca(wavelength, epsMatrix, epsMetal, f, r).first
 
@@ -85,39 +99,175 @@ object MetallicClusters {
 
         private fun alphaExtAlphaSca(wavelength: Double, epsMatrix: Complex_, epsMetal: Complex_, f: Double, r: Double): Pair<Double, Double> {
 
-//            val n = Optics.toRefractiveIndex(epsMatrix)
-//            val m = epsMetal / epsMatrix
-//            val waveVector = n.real * 2.0 * PI / (wavelength * 1E-7)
-//
-//            println(Complex_(n.real * n.real) / (n * n))
-//
-//            return 3.0 * f * waveVector * ((m - 1.0) / (m + 2.0) * (n.real * n.real) / (n * n)).imaginary to 1000.0
+            init(wavelength, epsMatrix, epsMetal, r)
 
-            val a = r * 1E-7 // cm^-1 as for wavelength
-            val a2 = pow(a, 2.0)
-            val a3 = pow(a, 3.0)
+            val c1 = 2.0 * PI / (wavevector * wavevector)
 
-            val n = Optics.toRefractiveIndex(epsMatrix)
-            val waveVector = n.real * 2.0 * PI / (wavelength * 1E-7)
-            val x = waveVector * a
+            val Cext = c1 * a.subList(1, a.lastIndex).indices
+                    .sumByDouble { (2.0 * it + 1.0) * (a[it] + b[it]).real }
+            val Csca = c1 * a.subList(1, a.lastIndex).indices
+                    .sumByDouble { (2.0 * it + 1.0) * (pow(a[it].abs(), 2.0) + pow(b[it].abs(), 2.0)) }
 
-            val x2 = x * x
-            val x4 = x * x * x * x
-            val m = epsMetal / epsMatrix // m_squared in Nolte paper
+//            println("$wavelength ${magnitude * 4.0 * PI * a2 * x * (c2).imaginary} ${magnitude * Cext2ndTerm}")
+//            println("$wavelength ${ampl * Cext1stTerm} ${ampl * Cext2ndTerm} ${c1.real} ${c1.imaginary}")
 
-            val c1 = (m - 1.0) / (m + 2.0)
-            val c2 = c1 * (n.real * n.real) / (n * n)
-            val c3 = 32.0 / 3.0 * PI * a2 * x4
-
-            val Cext = 4.0 * PI * a2 * x * (
-                    c2 * (ONE + c1 * x2 / 15.0 * (m * m + m * 27.0 + 38.0) / (m * 2.0 + 3.0))
-                    ).imaginary +
-                    c3 * (c2 * c1).real
-
-            val Csca = c3 * pow(c1.abs(), 2.0)
-
-            return with(3.0 / 4.0 * f / (PI * a3)) { this * Cext to this * Csca }
+            val c2 = 3.0 / 4.0 * f / (PI * pow(radius, 3.0))
+            return c2 * Cext to c2 * Csca
         }
+
+        private fun init(wavelength: Double, epsMatrix: Complex_, epsMetal: Complex_, r: Double) {
+            radius = r * 1E-7 // cm^-1 as for wavelength
+            val n = Optics.toRefractiveIndex(epsMatrix)
+            wavevector = n.real * 2.0 * PI / (wavelength * 1E-7)
+            x = wavevector * radius
+            m = Optics.toRefractiveIndex(epsMetal) / Optics.toRefractiveIndex(epsMatrix)
+            mx = m * x
+
+            nStop = Math.round(x + 4.0 * Math.pow(x, 1.0 / 3.0) + 2.0).toInt()
+
+            computeD()
+            println("************")
+            D.map { it.real }.forEach { println(it) }
+            D.map { it.imaginary }.forEach { println(it) }
+
+            computePsiChiXi()
+            computeab()
+        }
+
+        private fun computeD() {
+            val nMax = Math.round(Math.max(nStop.toDouble(), mx.abs())).toInt() + 15
+
+            println("$nStop $nMax")
+
+            D = IntArray(nMax).map { ZERO }.toMutableList()
+            for (i in nMax - 2 downTo 0) {
+                val c = Complex_(i.toDouble()) / mx
+                D[i] = c - ONE / (D[i + 1] + c)
+            }
+        }
+
+        private fun computePsiChiXi() {
+            psi = DoubleArray(nStop).toMutableList()
+            psi[0] = cos(x)
+            psi[1] = sin(x)
+
+            val chi = DoubleArray(nStop).toMutableList()
+            chi[0] = -sin(x)
+            chi[1] = cos(x)
+
+            for (i in 2 until nStop) {
+                psi[i] = (2.0 * i - 1.0) / x * psi[i - 1] - psi[i - 2]
+                chi[i] = (2.0 * i - 1.0) / x * chi[i - 1] - chi[i - 2]
+            }
+
+            xi = psi.indices.map { Complex_(psi[it], -chi[it]) }
+        }
+
+        private fun computeab() {
+            a = DoubleArray(nStop).map { ZERO }.toMutableList()
+            b = DoubleArray(nStop).map { ZERO }.toMutableList()
+            for (i in 1 until nStop) {
+                val aCommon = D[i] / m + i / x
+                a[i] = (aCommon * psi[i] - psi[i - 1]) / (aCommon * xi[i] - xi[i - 1])
+
+                val bCommon = D[i] * m + i / x
+                b[i] = (bCommon * psi[i] - psi[i - 1]) / (bCommon * xi[i] - xi[i - 1])
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+//        fun extinctionCoefficient(wavelength: Double, epsMatrix: Complex_, epsMetal: Complex_, f: Double, r: Double) =
+//                alphaExtAlphaSca(wavelength, epsMatrix, epsMetal, f, r).first
+//
+//        fun scatteringCoefficient(wavelength: Double, epsMatrix: Complex_, epsMetal: Complex_, f: Double, r: Double) =
+//                alphaExtAlphaSca(wavelength, epsMatrix, epsMetal, f, r).second
+//
+//        private fun alphaExtAlphaSca(wavelength: Double, epsMatrix: Complex_, epsMetal: Complex_, f: Double, r: Double): Pair<Double, Double> {
+//
+//            // r independent
+////            val n = Optics.toRefractiveIndex(epsMatrix)
+////            val m = epsMetal / epsMatrix
+////            val waveVector = n.real * 2.0 * PI / (wavelength * 1E-7)
+////
+////            println("$wavelength ${3.0 * f * waveVector * ((m - 1.0) / (m + 2.0) * (n.real * n.real) / (n * n)).imaginary}")
+////
+////            return 3.0 * f * waveVector * ((m - 1.0) / (m + 2.0)).imaginary to 1000.0
+//////            return 3.0 * f * waveVector * ((m - 1.0) / (m + 2.0) * (n.real * n.real) / (n * n)).imaginary to 1000.0
+//
+//
+//            val a = r * 1E-7 // cm^-1 as for wavelength
+//            val a2 = pow(a, 2.0)
+//            val a3 = pow(a, 3.0)
+//
+//            val n = Optics.toRefractiveIndex(epsMatrix)
+//            val x = n.real * 2.0 * PI / (wavelength * 1E-7) * a
+//
+//            val x2 = pow(x, 2.0)
+//            val x4 = pow(x, 4.0)
+//            val m = epsMetal / epsMatrix // m_squared in Nolte paper
+//
+//            val c1 = (m - 1.0) / (m + 2.0)
+//            val c2 = c1 * (n.real * n.real) / (n * n)
+//            val c3 = 32.0 / 3.0 * PI * a2 * x4
+//
+//            val Cext1stTerm =
+//                    4.0 * PI * a2 * x * (c2 * (ONE + c1 * x2 / 15.0 * (m * m + m * 27.0 + 38.0) / (m * 2.0 + 3.0))).imaginary
+//            val Cext2ndTerm = c3 * (c2 * c1).real
+//            val Cext = Cext1stTerm + Cext2ndTerm
+//
+//            val ampl = 3.0 / 4.0 * f / (PI * a3)
+//
+//            println("$wavelength ${ampl * 4.0 * PI * a2 * x * (c2).imaginary} ${ampl * Cext2ndTerm}")
+////            println("$wavelength ${ampl * Cext1stTerm} ${ampl * Cext2ndTerm} ${c1.real} ${c1.imaginary}")
+//
+//            val Csca = c3 * pow(c1.abs(), 2.0)
+//
+//            return with(3.0 / 4.0 * f / (PI * a3)) { this * Cext to this * Csca }
+//
+//
+//
+////            val n = Optics.toRefractiveIndex(epsMatrix)
+////            val rReduced = r * 1E-7 // cm^-1 as for wavelength
+////            val rReduced3 = pow(rReduced, 3.0)
+////            val wavelengthReduced = wavelength * 1E-7
+////            val waveVector = n * 2.0 * PI / wavelengthReduced
+////            val waveVectorReal = n.real * 2.0 * PI / wavelengthReduced
+////
+////            val x = waveVector * rReduced
+////
+////            val x3 = x * x * x
+////            val x5 = x * x * x * x * x
+////            val x6 = x * x * x * x * x * x
+////            val m = epsMetal / epsMatrix // m_squared in Nolte paper
+////
+////            val c1 = m - 1.0
+////            val c2 = c1 / (m + 2.0)
+////
+////            val a = listOf(
+////                    -I * 2.0 / 3.0 * x3 * c2 - I * 2.0 / 5.0 * x5 * c2 * (m - 2.0) / (m + 2.0) + c2 * c2 * 4.0 / 6.0 * x6,
+////                    -I / 15.0 * x5 * c1 / (m * 2.0 + 3.0)
+////            )
+////
+////            val b = listOf(
+////                    -I / 45.0 * x5 * c1,
+////                    ZERO
+////            )
+////
+//////            val c3 = Complex_(2.0 * PI) / (waveVector * waveVector)
+////            val c3 = 2.0 * PI / (waveVectorReal * waveVectorReal)
+////            val Cext = c3 * a.indices.sumByDouble { (2 * it + 1) * (a[it] + b[it]).real }
+////            val Csca = c3 * a.indices.sumByDouble { (2 * it + 1) * (pow(a[it].abs(), 2.0) + pow(b[it].abs(), 2.0)) }
+////
+////            return with(3.0 / 4.0 * f / (PI * rReduced3)) { this * Cext to this * Csca }
+//        }
     }
 
     /**
